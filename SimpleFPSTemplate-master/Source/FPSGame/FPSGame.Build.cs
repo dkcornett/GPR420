8605675b8d7ff3a4652f0c0a82dfa4cb69d93d37
Dianna Cornett <dianna.cornett@mymail.champlain.edu>
2021-03-22 21:59:34 -0400

    tweaks setting up for API functionality

--- SimpleFPSTemplate-master/Source/FPSGame/FPSGame.Build.cs
+++ SimpleFPSTemplate-master/Source/FPSGame/FPSGame.Build.cs
@@ -8,6 +8,6 @@ public class FPSGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Http", "Json", "JsonUtilities" });
 	}
 }
